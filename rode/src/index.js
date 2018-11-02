56620db1ff9798a53b6c0c14b92dfcab1a963b8c
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-02 10:42:51 +0800

    add webpack alias

--- rode/src/index.js
+++ rode/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import '@/assets/reset.less'
 
-import hacks from 'viewport-units-buggyfill/viewport-units-buggyfill.hacks'
-import ViewportUnitsBuggyfill from 'viewport-units-buggyfill'
+// import hacks from 'viewport-units-buggyfill/viewport-units-buggyfill.hacks'
+// import ViewportUnitsBuggyfill from 'viewport-units-buggyfill'
 
-ViewportUnitsBuggyfill.init({hacks: hacks})
+// ViewportUnitsBuggyfill.init({hacks: hacks})
 
 ReactDOM.render(<App />, document.getElementById('root'))
