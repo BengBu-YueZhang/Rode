b49905a251f94689b4e53c3d17b5cd4d0d4a37f3
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-02 10:34:27 +0800

    add viewport-units-buggyfill

--- rode/src/index.js
+++ rode/src/index.js
@@ -2,4 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import hacks from 'viewport-units-buggyfill/viewport-units-buggyfill.hacks'
+import ViewportUnitsBuggyfill from 'viewport-units-buggyfill'
+
+ViewportUnitsBuggyfill.init({hacks: hacks})
+
 ReactDOM.render(<App />, document.getElementById('root'))
