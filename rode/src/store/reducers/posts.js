114a9ea08f1a1b0f806cadc78604f0ba2b69d990
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-06 17:18:51 +0800

    update

--- rode/src/store/reducers/posts.js
+++ rode/src/store/reducers/posts.js
@@ -13,7 +13,7 @@ function posts (state = init, action) {
       return state.withMutations(function (state) {
         state.set('loading', false)
         .update('list', list => action.more ? list.concat(action.data) : list = List(action.data))
-        .update('loadedAll', loadedAll => action.data.length ? false : true)
+        .update('loadedAll', loadedAll => action.data.length ? loadedAll = false : loadedAll = true)
       })
     case actions.POST_ERROR:
       return state.withMutations(function (state) {
