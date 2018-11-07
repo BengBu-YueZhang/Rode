83b0ea922116be118919b99426bf7334217abefe
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-07 22:45:49 +0800

    add dist

---
+++ rode/build/precache-manifest.6ac9d9c79355e70d2c307e0d8752c0bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "da0a55f53826927a01f2",
+    "url": "/static/js/6.da0a55f5.chunk.js"
+  },
+  {
+    "revision": "acc1714f157cb733fab5",
+    "url": "/static/js/0.acc1714f.chunk.js"
+  },
+  {
+    "revision": "fe5748719dbaa49785b3",
+    "url": "/static/js/runtime~main.fe574871.js"
+  },
+  {
+    "revision": "4ef7b9d40e98f6da5960",
+    "url": "/static/js/main.4ef7b9d4.chunk.js"
+  },
+  {
+    "revision": "5d12df4b927dc5b63c0d",
+    "url": "/static/js/3.5d12df4b.chunk.js"
+  },
+  {
+    "revision": "8f8d8f4d47eb08400438",
+    "url": "/static/js/4.8f8d8f4d.chunk.js"
+  },
+  {
+    "revision": "3c4521af0aa59e242a62",
+    "url": "/static/js/5.3c4521af.chunk.js"
+  },
+  {
+    "revision": "688255bfb7296c266fbb",
+    "url": "/static/js/1.688255bf.chunk.js"
+  },
+  {
+    "revision": "b655428f50df86c4fabd",
+    "url": "/static/js/7.b655428f.chunk.js"
+  },
+  {
+    "revision": "b4edc8cb53cb2004abac",
+    "url": "/static/js/8.b4edc8cb.chunk.js"
+  },
+  {
+    "revision": "d5ccb62c07b55addb8dc",
+    "url": "/static/js/9.d5ccb62c.chunk.js"
+  },
+  {
+    "revision": "347b1daa917ed41706df",
+    "url": "/static/js/10.347b1daa.chunk.js"
+  },
+  {
+    "revision": "4ef7b9d40e98f6da5960",
+    "url": "/static/css/main.63a1c154.chunk.css"
+  },
+  {
+    "revision": "c4897d1d795a5f7cea03fe5b8ab9292e",
+    "url": "/index.html"
+  }
+];
